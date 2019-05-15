877bbcb80584e1aa7f281b542e4d2aaa9e9840c7
MarshallRawson <marshallrawson@ufl.edu>
2019-05-15 12:48:57 -0500

    incremental

--- simulation/mil_gazebo/include/mil_gazebo/mil_image_ground_truth.hpp
+++ simulation/mil_gazebo/include/mil_gazebo/mil_image_ground_truth.hpp
@@ -2,18 +2,18 @@
 #define MIL_IMAGE_GROUND_TRUTH_HH
 
 #include <string>
-#include <ros/ros.h>
-//#include <tf/transform_listener.h>
+#include <vector>
 
+#include <ros/ros.h>
 #include <gazebo/plugins/CameraPlugin.hh>
-//#include <gazebo_plugins/gazebo_ros_camera_utils.h>
-//#include <gazebo_plugins/gazebo_ros_camera.h>
+#include <gazebo/common/common.hh>
+#include <gazebo/physics/physics.hh>
+#include <sensor_msgs/Image.h>
+#include <image_geometry/pinhole_camera_model.h>
+#include <tf/transform_listener.h>
 
 #include <mil_msgs/ObjectsInImage.h>
 #include <mil_msgs/ObjectInImage.h>
-#include <gazebo/common/common.hh>
-#include <gazebo/physics/physics.hh>
-#include <vector>
 
 namespace mil_gazebo
 {
@@ -21,27 +21,34 @@ class MilImageGroundTruth : public gazebo::CameraPlugin
 {
   private:
     ros::NodeHandle n_;
-    ros::Publisher pub_; 
-    gazebo::physics::Model_V   models_;
-    std::vector<ignition::math::Vector2i> projections_;
-  public: MilImageGroundTruth() : gazebo::CameraPlugin()
-  {
-    pub_ = n_.advertise<ObjectsInImage>("marshalls_cool_topic", 1000);
-  }
+    ros::Publisher objects_pub_;
+
+    ros::Publisher debug_pub_map_;
+    ros::Publisher debug_pub_cam_;
+
+    ros::Subscriber image_sub_;
+    ros::Subscriber camera_info_sub_;
+    sensor_msgs::CameraInfoConstPtr cam_info_;
+    image_geometry::PinholeCameraModel camera_;
+    tf::TransformListener tf_listener_;
+
+  public: MilImageGroundTruth() : gazebo::CameraPlugin(), tf_listener_(ros::Duration(2.0)){}
 
   public: ~MilImageGroundTruth(){}
 
   public: 
     void Load(gazebo::sensors::SensorPtr _parent, 
-              sdf::ElementPtr            _sdf) override
-    { 
-      gazebo::CameraPlugin::Load(_parent, 
-                                 _sdf);
-    }
-  protected:
+              sdf::ElementPtr            _sdf);
+/*  protected:
     void OnNewFrame(const unsigned char *_image,
                           unsigned int   _width, unsigned int       _height,
                           unsigned int   _depth, const std::string &_format);
+*/
+
+  protected:
+    void CameraInfoCB(const sensor_msgs::CameraInfoConstPtr& _cam_info);
+  protected:
+    void ImageCB(const sensor_msgs::Image& msg);
 
 };
 GZ_REGISTER_SENSOR_PLUGIN(MilImageGroundTruth);
--- simulation/mil_gazebo/src/mil_image_ground_truth.cpp
+++ simulation/mil_gazebo/src/mil_image_ground_truth.cpp
@@ -1,35 +1,130 @@
 #include<mil_gazebo/mil_image_ground_truth.hpp>
 namespace mil_gazebo
 {
+void MilImageGroundTruth::Load(gazebo::sensors::SensorPtr _parent,
+                               sdf::ElementPtr            _sdf)
+{
+  gazebo::CameraPlugin::Load(_parent,
+                             _sdf);
+  std::string image_topic_name = "image_raw";
+  if (_sdf->HasElement("imageTopicName"))
+    image_topic_name = _sdf->Get<std::string>("imageTopicName");
+  std::string camera_info_topic_name = "camera_info";
+  if (_sdf->HasElement("cameraInfoTopicName"))
+    camera_info_topic_name = _sdf->Get<std::string>("cameraInfoTopicName");
 
+  image_sub_ = n_.subscribe(image_topic_name, 1, &MilImageGroundTruth::ImageCB, this);
+  camera_info_sub_ = n_.subscribe(camera_info_topic_name, 1, &MilImageGroundTruth::CameraInfoCB, this);
+  objects_pub_ = n_.advertise<mil_msgs::ObjectsInImage>("/image_ground_truth/"+(parentSensor->Name()), 1000);
+
+  debug_pub_map_ = n_.advertise<geometry_msgs::PointStamped>("/DEBUG/"+(parentSensor->Name())+"/map", 1);
+  debug_pub_cam_ = n_.advertise<geometry_msgs::PointStamped>("/DEBUG/"+(parentSensor->Name())+"/cam", 1);
+}
+
+/*
 void MilImageGroundTruth::OnNewFrame(const unsigned char *_image,
                                      unsigned int _width, unsigned int _height,
                                      unsigned int _depth, const std::string &_format)
 {
-  models_ = gazebo::physics::get_world(parentSensor->WorldName())->GetModels();
+  object_in_image_.clear();
+  gazebo::physics::Model_V models =gazebo::physics::get_world(parentSensor->WorldName())->GetModels(); 
+  object_in_image_.resize(models.size());
+  
   {
-  auto i=models_.begin();
-  auto j=projections_.begin();
-  for(;i!=models_.end(),j!=projections_.end();++i,++j)
+  auto i = object_in_image_.begin();
+  for (auto j = models.begin(); j!=models.end(); ++j, ++i)
   {
+    i->name = (*j)->GetName();
     ignition::math::Vector3d v
     (
-      (*i)->GetWorldPose().pos.x,
-      (*i)->GetWorldPose().pos.y,
-      (*i)->GetWorldPose().pos.z
+      (*j)->GetWorldPose().pos.x,
+      (*j)->GetWorldPose().pos.y,
+      (*j)->GetWorldPose().pos.z
     );
-    *j = parentSensor->Camera()->Project(v);
+    i->confidence = 1.0;
+    i->points.resize(1);
+    i->points.at(0).x = parentSensor->Camera()->Project(v)[0];
+    i->points.at(0).y = parentSensor->Camera()->Project(v)[1];
+    
   }
   }
+  return;
+}
+*/
 
-  if (!ros::isInitialized())
+void MilImageGroundTruth::CameraInfoCB(const sensor_msgs::CameraInfoConstPtr& _cam_info)
+{
+  cam_info_=_cam_info;
+}
+
+void MilImageGroundTruth::ImageCB(const sensor_msgs::Image& _msg)
+{
+  camera_.fromCameraInfo(cam_info_);
+  tf::StampedTransform tf;
+  try
   {
-    ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized, unable to load plugin. "
-      << "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in the gazebo_ros package)");
+    tf_listener_.waitForTransform(camera_.tfFrame(), "map" , _msg.header.stamp, ros::Duration(1.0));
+    tf_listener_.lookupTransform(camera_.tfFrame(), "map", _msg.header.stamp, tf);
+  }
+  catch (tf::TransformException ex)
+  {
+    ROS_ERROR("%s",ex.what());
     return;
   }
-  ObjectsInImage objects_in_image();
+  catch (tf::LookupException ex)
+  {
+    ROS_ERROR("%s",ex.what());
+    return;
+  }/*
+  catch (tf::ExtrapolationException ex)
+  {
+    ROS_ERROR("%s",ex.what());
+    return;
+  }*/
+  mil_msgs::ObjectsInImage objects_in_image;
+  objects_in_image.header = _msg.header;
   
+  for(gazebo::physics::ModelPtr i : gazebo::physics::get_world(parentSensor->WorldName())->GetModels()) 
+  {
+    mil_msgs::ObjectInImage object_in_image;
+    object_in_image.confidence = 1.0;
+    object_in_image.name = i->GetName();
+
+    geometry_msgs::PointStamped p;
+    p.header.stamp = _msg.header.stamp;
+    p.header.frame_id = "map";
+
+    p.point.x = i->GetWorldPose().pos.x;
+    p.point.y = i->GetWorldPose().pos.y;
+    p.point.z = i->GetWorldPose().pos.z;
+
+    geometry_msgs::PointStamped pPrime;
+    pPrime.header.stamp = _msg.header.stamp;
+    pPrime.header.frame_id = camera_.tfFrame();
+    
+    tf_listener_.transformPoint(camera_.tfFrame(), p, pPrime);
+    if (i->GetName() == "dice")
+    {
+      debug_pub_map_.publish(p);
+      debug_pub_cam_.publish(pPrime);
+    }
+    cv::Point3d pPrimeCV;
+    pPrimeCV.x = pPrime.point.x;
+    pPrimeCV.y = pPrime.point.y;
+    pPrimeCV.z = pPrime.point.z;
+
+    cv::Point2d q = camera_.project3dToPixel(pPrimeCV);
+
+    object_in_image.points.resize(1);
+    object_in_image.points.at(0).x = q.x;
+    object_in_image.points.at(0).y = q.y;
+
+
+    objects_in_image.objects.push_back(object_in_image);
+  }
+   
+  objects_pub_.publish(objects_in_image);
+
   return;
 }
 }
